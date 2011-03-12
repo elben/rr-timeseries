726a37f5abe3bd2cc0c897c7e52a309c10138c3e
Elben Shira <elbenshira@gmail.com>
2011-03-12 14:39:51 -0800

    Make everything public; #getkey by defaults goes to Time.now

--- timeseries.rb
+++ timeseries.rb
@@ -1,8 +1,14 @@
 class Timeseries
+  # Create a new Timeseries object.
+  #   redis - the redis object used
+  #   label - redis keys will be of the format "tseries:#{label}:..."
+  #   timestep - number of seconds in a series. Pass in -1 for an infinite time
+  #              step.
   def initialize(redis, label, timestep=60)
     @redis = redis
     @label = label
-    @timestep = timestep
+    @timestep = [-1, timestep].max
+
   end
 
   def incr(field)
@@ -10,25 +16,39 @@ class Timeseries
   end
   
   def incrby(field, n)
-    t = Time.now
-    @redis.hincrby(getkey(t), field, n)
+    @redis.hincrby(getkey, field, n)
   end
 
+  # Get the count for the series at start_time.
+  # By default, returns the current count.
   def get(field, start_time=nil)
     start_time ||= Time.now
     normalize_count(@redis.hget(getkey(start_time), field))
   end
 
-  def get_last(field)
+  # Get the last `last` counts ordered from oldest to earliest in time.
+  def get_last(field, last=1)
     t = Time.now
-    t_prev = t - @timestep
-    normalize_count(@redis.hget(getkey(t_prev), field))
+
+    if @timestep == -1
+      return [normalize_count(@redis.hget(getkey(t), field))]
+    end
+
+    vals = []
+    last.times do |l|
+      t -= @timestep
+      vals << normalize_count(@redis.hget(getkey(t), field))
+    end
+    vals.reverse
   end
 
-private
-
-  def getkey(time)
-    "tseries:#{@label}:#{normalize_time time}"
+  def getkey(time=nil)
+    time ||= Time.now
+    if @timestep == -1
+      "tseries:#{@label}:all_time"
+    else
+      "tseries:#{@label}:#{normalize_time time}"
+    end
   end
 
   def normalize_time(time)
@@ -40,3 +60,4 @@ private
       count.to_i # if nil, we get 0
   end
 end
+
